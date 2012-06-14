28050cfb14a22deb51b0c8089e8e000cfe3f5b51
Keno Fischer <kfischer@college.harvard.edu>
2012-06-14 19:47:58 +0200

    First commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Keno Fischer"
+maintainer_email "kfischer@college.harvard.edu"
+license          "MIT"
+description      "Configures the Jenkins server for use by the Julia Project"
+version          "0.1"
+depends "jenkins"
---
+++ recipes/default.rb
@@ -0,0 +1,24 @@
+julia_jenkins_plugins = ["git", "github-api", "github-oauth", "github"]
+
+include_recipe "jenkins"
+
+home_path = node[:jenkins][:server][:home]
+
+julia_jenkins_plugins.each do |name|
+    jenkins_plugin name do
+        action :create
+    end
+end
+
+directory "#{home_path}/config_sources" do
+    action :create
+end
+ 
+template "#{home_path}/config_sources/githubauth.xml" do
+    source "githubauth.xml.erb"
+end
+
+jenkins_config "#{home_path}/config.xml" do
+   local_file "#{home_path}/config_sources/githubauth.xml"
+   action :merge
+end
